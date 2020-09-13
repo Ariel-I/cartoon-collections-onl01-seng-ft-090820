979c65f2c6aea4fab77862474da9810a6d760d22
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-13 20:53:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"])
   i = 0 
   while i < 0 
-  puts dwarfs.each_with_index { |dwarfs, index| }
+  puts dwarfs.each_with_index { |index, dwarfs| }
 end
 
 def summon_captain_planet# code an argument here
