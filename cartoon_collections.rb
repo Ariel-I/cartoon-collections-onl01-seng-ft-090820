c1b028dcb181c9b84e439e3d01561af1874fc4c4
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-13 21:54:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  
-  # Your code here
+  calls = []
+  if calls >= 4.characters 
 end
 
 def find_the_cheese# code an argument here
