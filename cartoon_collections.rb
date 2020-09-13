4145140a68a742bfca4cb6133389311ac0a1c5ce
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-13 22:09:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.include? do |cheese|
+  array.find? do |cheese|
   
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
