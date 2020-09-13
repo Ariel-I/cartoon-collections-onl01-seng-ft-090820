acf121fc0662993845fbe20717e12eed4b948097
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-13 20:55:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"])
   rollcall = []
-  dwarfs.each_with_index{ |index, dwarfs| }
+  dwarfs.each_with_index{ |index, dwarfs| rollcall<< }
 end
 
 def summon_captain_planet# code an argument here
